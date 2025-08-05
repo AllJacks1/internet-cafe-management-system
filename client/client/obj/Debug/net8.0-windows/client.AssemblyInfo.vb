c1acaeaab52b3da5d05682b4706337c3d1d10036
CodingMantis <k.tan.531880@umindanao.edu.ph>
2025-08-05 17:32:43 +0800

    project setup
    -uploading initial files

--- client/client/obj/Debug/net8.0-windows/client.AssemblyInfo.vb
+++ client/client/obj/Debug/net8.0-windows/client.AssemblyInfo.vb
@@ -13,12 +13,12 @@ Option Explicit On
 
 Imports System
 Imports System.Reflection
-<Assembly: System.Reflection.AssemblyCompanyAttribute("internet-cafe-management-system"),  _
- Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
+<Assembly: System.Reflection.AssemblyCompanyAttribute("client"),  _
+ Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a40dcf355eda04212450d04aa4ed524c10b783"),  _
- Assembly: System.Reflection.AssemblyProductAttribute("internet-cafe-management-system"),  _
- Assembly: System.Reflection.AssemblyTitleAttribute("internet-cafe-management-system"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13dfe6f64b9d665d628c40d0a5e347ed977cc04"),  _
+ Assembly: System.Reflection.AssemblyProductAttribute("client"),  _
+ Assembly: System.Reflection.AssemblyTitleAttribute("client"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
  Assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0"),  _
  Assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")> 
