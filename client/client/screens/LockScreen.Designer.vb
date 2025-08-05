7bbb7dee9fde943ae8335e76bdae88053fe0fc0b
CodingMantis <k.tan.531880@umindanao.edu.ph>
2025-08-05 22:03:56 +0800

    lockscreen design

--- client/client/screens/LockScreen.Designer.vb
+++ client/client/screens/LockScreen.Designer.vb
@@ -22,17 +22,64 @@ Partial Class LockScreen
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LockScreen))
+        PictureBox1 = New PictureBox()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1920, 1080)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.FromArgb(CByte(115), CByte(115), CByte(115))
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.ForeColor = Color.FromArgb(CByte(217), CByte(217), CByte(217))
+        TextBox1.Location = New Point(766, 771)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(394, 28)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "Username"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.FromArgb(CByte(115), CByte(115), CByte(115))
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.ForeColor = Color.FromArgb(CByte(217), CByte(217), CByte(217))
+        TextBox2.Location = New Point(766, 839)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(394, 28)
+        TextBox2.TabIndex = 1
+        TextBox2.Text = "Password"
         ' 
         ' LockScreen
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1920, 1080)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(PictureBox1)
         FormBorderStyle = FormBorderStyle.None
         Name = "LockScreen"
         Text = "ICMS"
         WindowState = FormWindowState.Maximized
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- client/client/screens/LockScreen.vb
+++ client/client/screens/LockScreen.vb
@@ -1,3 +1,11 @@
 ﻿Public Class LockScreen
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Clear()
+        TextBox1.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(255))
+    End Sub
 
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+        TextBox2.Clear()
+        TextBox2.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(255))
+    End Sub
 End Class
